2f93487a5d616ada29b9c6720dfed24db6995ba8
CHRIS_LEE <chrismlee87@gmail.com>
2020-09-30 21:36:10 -0700

    master

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
-asdasdasd
+# You will store your poem in a .txt file. Feel free to use any poem of your choice!
+
+# Create a function called get_file_lines():
+
+# It should have 1 parameter called filename, which is a string of the filename.
+# It should return a list of strings containing the lines of the file.
+# Create a function called lines_printed_backwards():
+
+# It should have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
+
+# It should print the lines of the poem in reverse. Include the original line number at the beginning of each line.
+
+# For example, if you poem is Remember by Joy Harjo:
+
+
+
+def get_file_lines(filename):
+
+
+def lines_printed_backwards(lines_list):
+
+
+def lines_printed_random(lines_list):
+
+
+
+def lines_printed_custom(lines_list):
+
+
+
+
+get_file_lines()
+lines_printed_backwards()
+lines_printed_random()
+lines_printed_custom()
