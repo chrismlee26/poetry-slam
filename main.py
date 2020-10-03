c9a3b4f29c8624acc2ca85004eb83ca0ec54ba4e
CHRIS_LEE <chrismlee87@gmail.com>
2020-10-02 17:25:32 -0700

    first 2 working

--- main.py
+++ main.py
@@ -5,31 +5,39 @@
 # It should have 1 parameter called filename, which is a string of the filename.
 # It should return a list of strings containing the lines of the file.
 # Create a function called lines_printed_backwards():
-
 # It should have 1 parameter called lines_list, which is a list of strings containing lines of your poem.
-
-# It should print the lines of the poem in reverse. Include the original line number at the beginning of each line.
-
-# For example, if you poem is Remember by Joy Harjo:
 
 
 
 def get_file_lines(filename):
+    file_lines = open(filename, "r")
+    for x in file_lines:
+        print(x)
 
+get_file_lines("poem.txt")
+
+#It should print the lines of the poem in reverse. Include the original line number at the beginning of each line.
 
 def lines_printed_backwards(lines_list):
+    with open(lines_list, "r") as read_obj:
+        lines = read_obj.readlines()
+        lines = [line.strip() for line in lines]
+        lines = reversed(lines)
+        return lines
+
+lines_backwards = lines_printed_backwards("poem.txt")
+
+for line in lines_backwards:
+    print(line)
 
 
-def lines_printed_random(lines_list):
+# lines_printed_backwards("poem.txt")
+
+# def lines_printed_random(lines_list):
 
 
 
-def lines_printed_custom(lines_list):
+# def lines_printed_custom(lines_list):
 
 
 
-
-get_file_lines()
-lines_printed_backwards()
-lines_printed_random()
-lines_printed_custom()
